f7e7d1c6fd96d037693bb935b344848fa36dc677
郑玉叠 <zhengyudie@yf-networks.com>
2026-08-20 17:52:45 +0800

    修改版权信息

--- src/modules/ModelPrices/components/ModelPriceView.vue
+++ src/modules/ModelPrices/components/ModelPriceView.vue
@@ -1,4 +1,4 @@
-/** * Copyright(c) 2026 The rainway-ai-gateway Authors. * * Licensed under the
+/** * Copyright(c) 2026 The Rainway AI Gateway (壬远AI网关) Authors.* * Licensed under the
 Apache License, Version 2.0 (the "License"); * you may not use this file except
 in compliance with the License. * You may obtain a copy of the License at * *
 http: //www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable
